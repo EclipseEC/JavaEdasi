b4a543ddb9522c7cfde40608d333191ad15bc682
EclipseEC <andrei.proskurin91@gmail.com>
2015-03-25 13:26:56 +0200

    Uus klass Apple, mis loob õuna objekti.

---
+++ Praktikum2/src/ee/itcollege/snake/parts/Apple.java
@@ -0,0 +1,31 @@
+package ee.itcollege.snake.parts;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+
+import ee.itcollege.snake.lib.IDrawable;
+
+public class Apple implements IDrawable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int APPLE_SIZE  = 20;
+	int x = 100;
+	int y = 100;
+
+	@Override
+	public Area getArea() {
+		return new Area(new Ellipse2D.Double(x, y, APPLE_SIZE, APPLE_SIZE));
+	}
+
+	@Override
+	public void drawItself(Graphics2D g) {
+		g.setColor(Color.red);
+		g.fill(getArea());
+	}
+
+}
--- Praktikum2/src/ee/itcollege/snake/parts/GameField.java
+++ Praktikum2/src/ee/itcollege/snake/parts/GameField.java
@@ -13,50 +13,43 @@ import javax.swing.JApplet;
  */
 @SuppressWarnings("serial")
 public class GameField extends JApplet {
-    
-    private Snake snake = new Snake();
-    private Image buffer;
-    
-    private Image getBuffer() {
-    	if (null == buffer) {
-    		// if there is no buffer object, create a new Image
-    		buffer = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-    	}
-    	// fill the image with white
-    	Graphics g = buffer.getGraphics();
-        g.setColor(Color.white);
-        g.fillRect(0, 0, getWidth(), getHeight());
-        
-        return buffer;
-    }
-    
-    @Override
-    public void init() {
-    	System.err.println("Please run the SnakeGameStart");
-    	System.exit(0);
-    }
-    
-    @Override
-    public void paint(Graphics g) {
-    	Image buffer = getBuffer();
-        Graphics2D g2 = (Graphics2D) buffer.getGraphics();
 
-        snake.drawItself(g2);
-        
-        g.drawImage(buffer, 0, 0, null);
-    }
+	private Snake snake = new Snake();
+	private Apple apple = new Apple();
+	private Image buffer;
+
+	private Image getBuffer() {
+		if (null == buffer) {
+			// if there is no buffer object, create a new Image
+			buffer = new BufferedImage(getWidth(), getHeight(),
+					BufferedImage.TYPE_INT_ARGB);
+		}
+		// fill the image with white
+		Graphics g = buffer.getGraphics();
+		g.setColor(Color.white);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		return buffer;
+	}
+
+	@Override
+	public void init() {
+		System.err.println("Please run the SnakeGameStart");
+		System.exit(0);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Image buffer = getBuffer();
+		Graphics2D g2 = (Graphics2D) buffer.getGraphics();
+
+		snake.drawItself(g2);
+		apple.drawItself(g2);
+
+		g.drawImage(buffer, 0, 0, null);
+	}
 
 	public Snake getSnake() {
 		return snake;
 	}
 }
-
-
-
-
-
-
-
-
-
-
