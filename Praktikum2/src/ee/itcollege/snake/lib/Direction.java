017ad094c1de0f0f7e831f9934c795e7aa934232
EclipseEC <andrei.proskurin91@gmail.com>
2015-03-25 11:44:39 +0200

    isOpposite method

--- Praktikum2/src/ee/itcollege/snake/lib/Direction.java
+++ Praktikum2/src/ee/itcollege/snake/lib/Direction.java
@@ -2,4 +2,16 @@ package ee.itcollege.snake.lib;
 
 public enum Direction {
     LEFT, RIGHT, UP, DOWN;
+    
+    public static Direction isOpposite(Direction direction) {
+    	if (direction == DOWN) {
+    		return UP;
+    	} else if (direction == UP) {
+    		return DOWN;
+    	} else if (direction == LEFT) {
+    		return RIGHT;
+    	} else {
+    		return LEFT;
+    	}
+    }
 }
