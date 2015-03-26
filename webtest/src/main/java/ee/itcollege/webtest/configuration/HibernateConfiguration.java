553f140ec8f72e261ad3efb6668fe2ceb251fd0f
EclipseEC <andrei.proskurin91@gmail.com>
2015-03-26 17:34:35 +0200

    Pooling

--- webtest/src/main/java/ee/itcollege/webtest/configuration/HibernateConfiguration.java
+++ webtest/src/main/java/ee/itcollege/webtest/configuration/HibernateConfiguration.java
@@ -1,46 +1,54 @@
 package ee.itcollege.webtest.configuration;
 
+import java.beans.PropertyVetoException;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 public class HibernateConfiguration {
 
-    @Bean
-    public DataSource getDataSource() {
-        DriverManagerDataSource d = new DriverManagerDataSource();
-        d.setUrl("jdbc:h2:~/db/test");
-        d.setDriverClassName("org.h2.Driver");
-        return d;
-    }
-    
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(getDataSource());
-        emf.setPackagesToScan(new String[]{"ee.itcollege.webtest.entity"});
-        HibernateJpaVendorAdapter hibernateAdapter = new HibernateJpaVendorAdapter();
-        hibernateAdapter.setGenerateDdl(true);
-        emf.setJpaVendorAdapter(hibernateAdapter);
-        return emf;
-    }
-    
-    @Bean
-    public DataSourceTransactionManager txManager() {
-        return new DataSourceTransactionManager(getDataSource());
-    }
-    
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        return new JpaTransactionManager(entityManagerFactory().getObject());
-    }
-    
-    
+	@Bean
+	public DataSource getDataSource() {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setJdbcUrl("jdbc:h2:~/db/test");
+		dataSource.setMaxPoolSize(10);
+		dataSource.setMinPoolSize(2);
+		try {
+			dataSource.setDriverClass("org.h2.Driver");
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(getDataSource());
+		emf.setPackagesToScan(new String[] { "ee.itcollege.webtest.entity" });
+		HibernateJpaVendorAdapter hibernateAdapter = new HibernateJpaVendorAdapter();
+		hibernateAdapter.setGenerateDdl(true);
+		emf.setJpaVendorAdapter(hibernateAdapter);
+		return emf;
+	}
+
+	@Bean
+	public DataSourceTransactionManager txManager() {
+		return new DataSourceTransactionManager(getDataSource());
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		return new JpaTransactionManager(entityManagerFactory().getObject());
+	}
+
 }
