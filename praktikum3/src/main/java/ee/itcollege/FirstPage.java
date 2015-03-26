a86ab911aa6e4564ec788c3c9ccd5d738068c8e7
EclipseEC <andrei.proskurin91@gmail.com>
2015-03-26 16:06:58 +0200

    ListView

--- praktikum3/src/main/java/ee/itcollege/FirstPage.java
+++ praktikum3/src/main/java/ee/itcollege/FirstPage.java
@@ -1,40 +1,56 @@
 package ee.itcollege;
 
+import java.util.Arrays;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class FirstPage extends LayoutPage {
 	private static final long serialVersionUID = 1L;
-	
 	private int number = 0;
+	@SuppressWarnings("serial")
+	private Label label = new Label("number", new Model<Integer>() {
+		@Override
+		public Integer getObject() {
+			// TODO Auto-generated method stub
+			return number;
+		}
+	});
 
 	@SuppressWarnings("serial")
 	public FirstPage(final PageParameters parameters) {
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-		
+		add(new Label("version", getApplication().getFrameworkSettings()
+				.getVersion()));
+
 		add(new BookmarkablePageLink<Void>("second", SecondPage.class));
-		
-		add(new Label("number", new Model<Integer>() {
-			@Override
-			public Integer getObject() {
-				// TODO Auto-generated method stub
-				return number;
-			}
-		}));
-		
-		add(new Link<Void>("add") {
+
+		label.setOutputMarkupId(true);
+		add(label);
+
+		add(new AjaxLink<Void>("add") {
 
 			@Override
-			public void onClick() {
+			public void onClick(AjaxRequestTarget target) {
+				// TODO Auto-generated method stub
 				number++;
-				System.out.println(number);
+				target.add(label);
 			}
-			
-		}); 
+
+		});
+
+		add(new ListView<String>("list", Arrays.asList("test", "kala", "mati")) {
+			@Override
+			protected void populateItem(ListItem<String> item) {
+				item.add(new Label("name", item.getModelObject()));
+			}
+		});
 		// TODO Add your page's components here
 
-    }
+	}
 }
